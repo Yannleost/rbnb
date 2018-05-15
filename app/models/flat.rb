5b505ad21200a88231a4e64455957b5f48f21460
Yann Leost <yannleost@gmail.com>
2018-05-15 14:08:29 +0200

    fin du modèles et des validations"

--- app/models/flat.rb
+++ app/models/flat.rb
@@ -1,5 +1,17 @@
 class Flat < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id'
   has_many :reservations
+  validates :title, presence: true
+  validates :title, length: {minimum: 10, maximum: 120}, allow_blank: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates_numericality_of :price, :greater_than_or_equal_to => 0
+  validates :capacity, presence: true
+  validates_numericality_of :capacity, :greater_than_or_equal_to => 0
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :start_date, inclusion: { in: (Date.today..Date.new(2119,2,3)) }
+  validates :end_date, inclusion: { in: (Date.today..Date.new(2119,2,3)) }
+
 end
 
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,8 +1,11 @@
 class Reservation < ApplicationRecord
   belongs_to :flat
   belongs_to :client, class_name: 'User', foreign_key: 'user_id'
-
-# validates > pas de conflit avec d'autres résa + inclus dans la fourchette du flate
-# end
+  validates :start_date, presence: true # >= start_date du flat.
+  validates :start_date, inclusion: { in: (Date.today..Date.new(2119,2,3)) }
+  validates :end_date, presence: true # <= end_date du flat + > startdate resta
+  validates :end_date, inclusion: { in: (Date.today..Date.new(2119,2,3)) }
 
 end
+
+
