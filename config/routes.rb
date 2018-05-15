98362aab57302bd151a8ddbb231de3ec1a219d54
Ana Atanasova <fleurona@live.fr>
2018-05-15 16:41:06 +0200

    index et show finished du controller flat

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  # get 'flats/:id', to: "flats#show", as: "flat"
+  # get 'flats', to: "flats#index", as: "flats"
+  # <=>
+  resources :flats, only: [:index, :show]
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
