62439da9751958662a3f81a82499ef1b25d00692
Yann Leost <yannleost@gmail.com>
2018-05-15 16:39:12 +0200

    methode du controller faites par les mecs

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,35 @@
+class FlatsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.owner = current_user
+    @flat.save
+  end
+
+  def edit
+    @flat = Flat.find(params[:id])
+  end
+
+  def update
+    @flat = Flat.find(params[:id])
+    @flat.update(flat_params
+  end
+
+  def destroy
+    @flat = Flat.find(params[:id])
+    @flat.destroy
+    redirect_to root
+  end
+
+
+  private
+  def flat_params
+    params.require(:flat).permit(:title, :description, :price, :capacity, :start_date, :end_date)
+  end
+
+end
