ab1a80a1f5f98c30aa2f47fbd2e1eece12bc8cec
Yann Leost <yannleost@gmail.com>
2018-05-16 11:31:36 +0200

    card de index + finished seed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  has_many :flats
-  has_many :reservations
+  has_many :flats, dependent: :destroy
+  has_many :reservations, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 end
